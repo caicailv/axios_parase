44616ea6ef29e870082843464a0e678d6578a175
caicailv <1401394657@qq.com>
2021-11-03 15:37:22 +0800

    ''

--- sandbox/server.js
+++ sandbox/server.js
@@ -39,8 +39,6 @@ server = http.createServer(function (req, res) {
     pipeFileToResponse(res, '../dist/axios.map', 'text/javascript')
     } else if (pathname.includes('/api')) {
   // } else if (pathname === '/api') {
-    // } else if (pathname.includes('/api')) {
-  } else if (pathname === '/api') {
     var status
     var result
     var data = ''
